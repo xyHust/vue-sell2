9b738901c4e04d706afcee0bde723ef230c85815
15271936620 <2389651246@qq.com>
2019-08-18 21:20:03 +0800

    vue app project  is update

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,96 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-header :seller="seller"></v-header>
+    <div class="tab-wrapper">
+      <tab :tabs="tabs"></tab>
+    </div>
+
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script type="text/ecmascript-6">
+  import VHeader from 'components/v-header/v-header';
+  import { getSeller } from 'api'
+  import Goods from 'components/goods/goods'
+  import Ratings from 'components/ratings/ratings'
+  import Seller from 'components/seller/seller'
+  import Tab from 'components/tab/tab'
+  import qs from 'query-string'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+  const ERR_OK = 0;
+
+  export default {
+    name: 'app',
+    data() {
+      return {
+        seller: {
+          id: qs.parse(location.search).id
+        }
+      };
+    },
+    computed: {
+      tabs(){
+        return [
+          {
+            label:'商品',
+            component: Goods,
+            data: {
+              seller: this.seller
+            }
+          },
+          {
+            label:'评价',
+            component: Ratings,
+            data: {
+              seller: this.seller
+            }
+          },
+          {
+            label:'商家',
+            component: Seller,
+            data: {
+              seller: this.seller
+            }
+          }
+
+        ]
+      }
+    },
+    created() {
+      this._getSeller()
+
+    },
+    methods:{
+      _getSeller(){
+        getSeller({
+          id: this.seller.id
+        }).then((seller) => {
+          this.seller = seller
+        })
+      }
+    },
+    components: {
+      Tab,
+      VHeader
+    }
+  };
 </script>
-<style lang="stylus">
-#app
-  font-family 'Avenir', Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
+
+<style lang="stylus" rel="stylesheet/stylus">
+  @import "./common/stylus/mixin.styl"
+  .tab
+    display: flex
+    width: 100%
+    height: 40px
+    line-height: 40px
+    border-1px(rgba(7, 17, 27, 0.1))
+    .tab-item
+      flex: 1
+      text-align: center
+      & > a
+        display: block
+        font-size: 14px
+        color: rgb(77, 85, 93)
+        &.active
+          color: rgb(240, 20, 20)
 </style>
